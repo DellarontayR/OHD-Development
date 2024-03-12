217f81d7c7c0a0bbe14e7f99dcbf383dcf76f909
Dellarontay Readus <dellreadus@gmail.com>
2024-03-12 12:39:19 -0500

    Small updates, aligned headers footers css and js for publishing files

--- pubmodels.php
+++ pubmodels.php
@@ -25,22 +25,70 @@
   <!-- Page Icon -->
   <link rel="icon" href="Images/Color/1x/Social Media Icon.png" type="image/gif">
 
-    <title>OHD Inc. AgTech Model List</title>
+<title>OHD Inc. AgTech Model List</title>
+
+            <!-- Font Awesome icons (free version)-->
+            <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
   </head>
-<div class="container">
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <i class="fas fa-leaf"></i> &ensp;
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">OHD</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>   
+                </button>
+                <div>
+                    <a class="mx-2" href="https://twitter.com/OrganicHarvest4"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="https://www.facebook.com/Organic-Harvest-Digital-185758333096937"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="https://www.instagram.com/organic.harvest/"><i class="fab fa-instagram"></i></a></li>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto" style="text-align: center;">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="about.html">About</a>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.html">Product</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container d-flex h-100 align-items-center">
+                <div class="mx-auto text-center">
+                    <h1 class="mx-auto my-0 text-uppercase"></h1>
+                    <!-- <h2 class="text-white-50 mx-auto mt-2 mb-5">.</h2> -->
+                    <!-- <a class="btn btn-primary js-scroll-trigger" href="#about"></a> -->
+                    <br> </br>
+                </div>
+            </div>
+        </header>
 
-<div class="row">
-<?php foreach($models as $model) { ?>
-<h6 ><?php echo htmlspecialchars($model["modelName"]);  ?></h6>
-<h6 ><?php echo htmlspecialchars($model["summary"]);  ?></h6>
-<h6 ><?php echo htmlspecialchars($model["link"]);  ?></h6>
-<h6 ><?php echo htmlspecialchars($model["technologyUsed"]);  ?></h6>
-<h6 ><?php echo htmlspecialchars($model["licenseType"]);  ?></h6>
-
-
-<?php }?>
 </div>
 
-</div>
-
+<footer class="footer bg-black small text-center text-white-50">
+    <div class="social d-flex justify-content-center">
+            <a class="mx-2" href="https://twitter.com/OrganicHarvest4"><i class="fab fa-twitter"></i></a>
+            <a class="mx-2" href="https://www.facebook.com/Organic-Harvest-Digital-185758333096937"><i class="fab fa-facebook-f"></i></a>
+            <a class="mx-2" href="https://www.instagram.com/organic.harvest/"><i class="fab fa-instagram"></i></i></a>
+        </div>
+        <div>
+                    <span style="font-size:smaller; color: #f3bebf;">&copy;Organic Harvest Digital Inc. 2024<a class="text-white" href="#"></a><a class="text-white" href="#"></a></span>
+        </div>
+  </footer>
+      <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+</body>
 </html>
